1eaae9469ebb6f58fe33e37079a81c68058ee3fd
ZiwenYeee <42403302+ZiwenYeee@users.noreply.github.com>
2019-12-31 11:43:04 +0800

    improve efficiency

--- mv_test_implementation.py
+++ mv_test_implementation.py
@@ -4,19 +4,16 @@ import numpy as np
 import pandas as pd
 from numba import jit
 
-
 @jit(nopython = True)
-def mv_cal_sorted(x:np.array, y:np.array):
-    x_idx = np.argsort(x)
+def mv_cal_sorted(x:np.array, y:np.array, y_unique:float, n:float):
+    x_idx = np.argsort(x) #O(nlogn)
     y = y[x_idx]
-    y_unique = np.unique(y)
-    n = y.shape[0]
     mv_test = np.zeros((len(y_unique), ))
     y_array = np.zeros((len(y_unique), ))
     prior = np.zeros((len(y_unique), ))
+    y_num = 0
     for c in y_unique:
         prior[c] = np.sum(y == c) #O(p*n)
-    y_num = 0
     for row in range(n): #O(n)
         y_array[y[row]] += 1
         y_num += 1
@@ -26,21 +23,22 @@ def mv_cal_sorted(x:np.array, y:np.array):
     mv_test = np.sum(mv_test)
     return mv_test
 
+
 def mv_test_sorted(data:np.array, target:np.array):
     m = data.shape[1]
+    y_class = np.unique(target)
+    n = target.shape[0]
     res = Parallel(n_jobs = -1, verbose=0) \
-             (delayed(mv_cal_sorted)(data[:, col], target) 
+             (delayed(mv_cal_sorted)(data[:, col], target, y_class, n) 
               for col in range(m))
     return res
 
 def mv_feature_selection(data: pd.DataFrame, target:str, features:list, alpha = 0.001, feat_imp = False):
-    try:
-        from scipy import stats
-    except:
-        Exception("please pip install scipy.")
-    fe_used = np.array(data[features])
+    from scipy import stats
+
     target = np.array(data[target])
-    feat_val = mv_test_sorted(fe_used, target)
+    data = np.array(data[features])
+    feat_val = mv_test_sorted(data, target)
     R = len(np.unique(target))
     chi_value = stats.chi2.ppf(1 - alpha, R - 1)
     asymptotic = np.sum([chi_value/(np.pi ** 2 * j**2) for j in range(1, data.shape[0] + 1)])
@@ -58,4 +56,3 @@ def mv_feature_selection(data: pd.DataFrame, target:str, features:list, alpha = 
             c += 1
         return imp
     return feat_new
-
